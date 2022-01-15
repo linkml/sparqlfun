f12aa2fb8bd6647c031e31949872ed800be83a4b
Sujay Patil <sujaysanjeev.patil@gmail.com>
2022-01-14 16:59:54 -0800

    add auto publish github action

--- sparqlfun/__init__.py
+++ sparqlfun/__init__.py
@@ -1,1 +1,11 @@
 from sparqlfun.query_engine import SparqlEngine
+
+# use importlib.metadata to read the version provided
+# by the package during installation. Do not hardcode
+# the version in the code
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
